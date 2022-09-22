d4e4d287749254f7216f43e09c16fe356ad2280c
CheongHing <ych8815@gmail.com>
2022-09-22 16:58:12 +0800

    init

---
+++ resources/js/Pages/Tasks/List.vue
@@ -0,0 +1,35 @@
+<script setup>
+    import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+    import { Head } from '@inertiajs/inertia-vue3';
+    </script>
+
+    <template>
+        <Head title="Dashboard" />
+
+        <AuthenticatedLayout>
+            <template #header>
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    Tasks
+                </h2>
+            </template>
+
+            <div class="py-12">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <!-- <div class="p-6 bg-white border-b border-gray-200">
+                            You're logged in!
+                        </div> -->
+
+                        <table class="w-full">
+                            <tr>
+                                <th>#</th>
+                                <th>Job Ref</th>
+                                <th></th>
+                                <th>Action</th>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    </template>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,24 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/tasks', function () {
+    return Inertia::render('Tasks/List');
+})->middleware(['auth', 'verified'])->name('tasks');
+
+Route::get('/invoices', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('invoices');
+
+require __DIR__.'/auth.php';
