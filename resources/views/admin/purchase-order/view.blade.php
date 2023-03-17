834185297f0a1e561b644977c9fc61bbd5fa1d79
CheongHing <ych8815@gmail.com>
2023-03-17 17:08:59 +0800

    px

--- resources/views/admin/purchase-order/view.blade.php
+++ resources/views/admin/purchase-order/view.blade.php
@@ -326,12 +326,12 @@
                                 </tr>
                                 @foreach ($po->items as $item)
                                     <tr>
-                                        <td>{{ $loop->index +1 }}</td>
-                                        <td>{{ $item['title'] }}</td>
-                                        <td>{{ $item['description'] }}</td>
-                                        <td>{{ number_format($item['qty'], 2) }} {{ $item['unit'] }}</td>
-                                        <td>{{ $item['unit_price'] }}</td>
-                                        <td class="text-right">{{ number_format($item['qty'] * $item['unit_price'], 2) }}</td>
+                                        <td class="px-1">{{ $loop->index +1 }}</td>
+                                        <td class="px-1">{{ $item['title'] }}</td>
+                                        <td class="px-1">{{ $item['description'] }}</td>
+                                        <td class="px-1">{{ number_format($item['qty'], 2) }} {{ $item['unit'] }}</td>
+                                        <td class="px-1">{{ $item['unit_price'] }}</td>
+                                        <td class="text-right px-1">{{ number_format($item['qty'] * $item['unit_price'], 2) }}</td>
                                         {{-- <td class="text-right">
                                             {{
                                                 match ($item['direction']) {
