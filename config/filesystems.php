015ada6eb198eb9bebd92b9791a48e4ac4679fa0
ych8815 <ych8815@gmail.com>
2024-04-05 10:21:33 +0800

    755

--- config/filesystems.php
+++ config/filesystems.php
@@ -41,7 +41,7 @@ return [
                 ],
                 'dir' => [
                     'public' => 0755,
-                    'private' => 0700,
+                    'private' => 0755,
                 ],
             ],
         ],
@@ -59,7 +59,7 @@ return [
                 ],
                 'dir' => [
                     'public' => 0755,
-                    'private' => 0700,
+                    'private' => 0755,
                 ],
             ],
         ],
